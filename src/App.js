e574f6eef55aa1a74fab7261c8df0e8723e9bce9
Justin Gil <puppies4648@gmail.com>
2023-05-20 00:11:13 -0600

    home page layout done

--- src/App.js
+++ src/App.js
@@ -6,20 +6,25 @@ import { Home } from './pages/Home';
 import { Contact } from './pages/Contact';
 import { NotFound } from './pages/NotFound';
 import { Bar } from './components/Bar';
+import { ChakraProvider } from '@chakra-ui/react';
+
 
 
 function App() {
   return (
     <>
       <div className='app'>
-        <Bar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/works" element={<Works />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/about" element={<About />} />
-          <Route path='*' element={<NotFound />} />
-        </Routes>
+        <ChakraProvider>
+          <Bar />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/works" element={<Works />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="/about" element={<About />} />
+              <Route path='*' element={<NotFound />} />
+            </Routes>
+        </ChakraProvider>
+
       </div>
     </>
 
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,10 +1,49 @@
 import React from 'react';
 import "../styles/HomeStyles.scss";
+import { Box, Container, Grid, GridItem, Heading, Image} from '@chakra-ui/react';
+import logo from '../images/asa.jpg';
+import { Link } from 'react-router-dom';
 export const Home = () => {
   return (
     <>
       <div className='main-container'>
-        <h1>this is the main page</h1>
+        
+ 
+
+
+        <Container>
+        <Box boxSize='sm' className='main-image'>
+          <Image src={logo} alt='mainimage' />
+        </Box>
+        </Container>
+        <h3 className='letter-m'>M</h3>
+        <h3 className='letter-g'>G</h3>
+
+        <Grid className='grid-container'
+        h='100px'
+        templateRows='repeat(2, 1fr)'
+        templateColumns='repeat(3, 1fr)'
+        gap={3}
+        >
+          <GridItem >
+            <Container>
+               <Link to="/contact">Contact me</Link>
+            </Container>
+          </GridItem>
+          <GridItem >
+            <Link to="/works">All Works</Link>
+          </GridItem>
+          <GridItem >
+            <Link to="/about">About me</Link>
+          </GridItem>
+          <GridItem />
+          <GridItem >
+            <Link to="/collection">Collections</Link>
+          </GridItem>
+          <GridItem />
+        
+        </Grid>
+
       </div>
 
     </>
